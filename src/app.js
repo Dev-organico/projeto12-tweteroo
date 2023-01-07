0f2786ed281648da3c09e1e53027c6dfd0703b02
Dev-organico <joao.zolet@gmail.com>
2023-01-07 19:48:39 -0300

    posts e get pronto

--- src/app.js
+++ src/app.js
@@ -6,13 +6,18 @@ app.use(express.json())
 app.use(cors());
 const PORT = 5000
 
-const users = [
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-    }
-]
-const tweets = []
+const users = [{
+    username: 'bobesponja',
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+}]
+const tweets = [{
+    username: "bobesponja",
+    tweet: "eu amo o hub"
+},
+{
+    username: "bobesponja",
+    tweet: "eu odeio o hub"
+}]
 
 app.post("/sign-up", (req, res) => {
     const user = req.body
@@ -28,16 +33,37 @@ app.post("/tweets", (req, res) => {
     const tweet = req.body
 
 
-    if (!users.find(el => el.username == tweet.username)) return res.send("UNAUTHORIZED")
-
-
+    if (!users.find(el => el.username === tweet.username)) return res.send("UNAUTHORIZED")
 
     tweets.push(tweet)
     res.send("OK")
 
-
 })
 
+app.get("/tweets", (req, res) => {
+
+    const lastTen = (tweets.slice(-10, tweets.length))
+
+    let newLast = []
+
+    lastTen.forEach(el => {
+        users.find(i => {
+            if (el.username === i.username) {
+                newLast.push({
+                    username: i.username,
+                    avatar: i.avatar,
+                    tweet: el.tweet
+                })
+            }
+        })
+    })
+
+
+
+
+    res.send(newLast)
+
+})
 
 
 
